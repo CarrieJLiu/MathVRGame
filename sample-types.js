70b7261968c05a61137d4bc74f3745ec2a84bd7f
CarrieJLiu <98843320+CarrieJLiu@users.noreply.github.com>
2022-04-23 11:02:22 -0700

    Add files via upload

---
+++ sample-types.js
@@ -0,0 +1,13 @@
+var found = true;
+var grade = 88;
+var firstName = "anup";
+var lastName = 'liu';
+console.log(found);
+console.log("the grade is" + grade);
+console.log("hi" + firstName + " " + lastName);
+/*
+found = 0;
+grade = "A";
+firstName = false;
+*/
+console.log("hi ".concat(firstName, " ").concat(lastName));
