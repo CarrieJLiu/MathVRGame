cc99e11c76464a3ec545c982c64e31b434691f27
CarrieJLiu <98843320+CarrieJLiu@users.noreply.github.com>
2022-04-27 08:20:35 -0700

    Add files via upload

--- Customer.js
+++ Customer.js
@@ -1,11 +1,37 @@
-var Customer = /** @class */ (function () {
-    function Customer(theFirst, theLast) {
-        this.firstName = theFirst;
-        this.lastName = theLast;
+"use strict";
+class Customer {
+    constructor(theFirst, theLast) {
+        this._firstName = theFirst;
+        this._lastName = theLast;
     }
-    return Customer;
-}());
+    get firstName() {
+        return this._firstName;
+    }
+    set firstName(value) {
+        this._firstName = value;
+    }
+    get lastName() {
+        return this._lastName;
+    }
+    set lastName(value) {
+        this._lastName = value;
+    }
+}
 //create an instance
-var myCustomer = new Customer("liu", "jiali");
+let myCustomer = new Customer("liu", "jiali");
 console.log(myCustomer.firstName);
 console.log(myCustomer.lastName);
+/*
+class Customer {
+    firstName: string;
+    lastName: string;
+}
+
+let myCustomer = new Customer();
+
+myCustomer.firstName = "Jiali";
+myCustomer.lastName = "Liu";
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName0;)
+*/ 
--- Customer.ts
+++ Customer.ts
@@ -1,11 +1,27 @@
  class Customer {
-     firstName: string;
-     lastName: string;
+     private _firstName: string;
+     private _lastName: string;
+     
 
      constructor(theFirst: string, theLast: string) {
-         this. firstName = theFirst;
-         this.lastName = theLast;
+         this. _firstName = theFirst;
+         this._lastName = theLast;
      }
+
+     public get firstName(): string {
+         return this._firstName;
+     }
+
+     public set firstName(value: string) {
+         this._firstName = value;
+     }
+     
+     public get lastName(): string {
+        return this._lastName;
+    }
+    public set lastName(value: string) {
+        this._lastName = value;
+    }
  }
 
  //create an instance
