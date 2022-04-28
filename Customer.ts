d7a0a2b2d217e03f91d034cdfa00199cebea91c6
CarrieJLiu <98843320+CarrieJLiu@users.noreply.github.com>
2022-04-28 11:49:02 -0700

    Add files via upload

--- Customer.ts
+++ Customer.ts
@@ -1,11 +1,7 @@
  class Customer {
-     private _firstName: string;
-     private _lastName: string;
      
 
-     constructor(theFirst: string, theLast: string) {
-         this. _firstName = theFirst;
-         this._lastName = theLast;
+     constructor( private _firstName: string, private _lastName: string) {
      }
 
      public get firstName(): string {
